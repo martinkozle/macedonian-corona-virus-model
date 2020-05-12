5891a6f8c5c3bd2d6e7dd80006ae4e7c0863dcdb
martinkozle <martinkozle@yahoo.com>
2020-05-12 17:46:49 +0200

    Reformatting to koronavirus_gov_mk_scrapper, made some variables private

--- koronavirus_gov_mk_scrapper.py
+++ koronavirus_gov_mk_scrapper.py
@@ -10,19 +10,19 @@ import csv
 class Scrapper:
 
     def __init__(self, *, debug=False):
-        self.site_url = 'https://koronavirus.gov.mk/stat'
         self.data = []
-        self.debug = debug
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(level=logging.DEBUG if debug else logging.INFO)
+        self._site_url = 'https://koronavirus.gov.mk/stat'
+        self._debug = debug
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(level=logging.DEBUG if debug else logging.INFO)
         handler = logging.StreamHandler(sys.stdout)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        self._logger.addHandler(handler)
 
     # returns a Selenium webdriver based on headless Chrome
     @staticmethod
-    def headless_chrome_driver():
+    def _headless_chrome_driver() -> webdriver.Chrome:
         options = webdriver.ChromeOptions()
         options.add_argument("headless")
         driver = webdriver.Chrome(options=options)
@@ -30,44 +30,44 @@ class Scrapper:
 
     # full chrome webdriver for debugging
     @staticmethod
-    def chrome_driver():
+    def _chrome_driver() -> webdriver.Chrome:
         return webdriver.Chrome()
 
-    def get_data(self):
+    def _get_data(self):
         driver = None
         response_body_json = None
         try:
-            self.logger.debug('Opening Chrome webdriver')
-            if self.debug:
-                driver = self.chrome_driver()
+            self._logger.debug('Opening Chrome webdriver')
+            if self._debug:
+                driver = self._chrome_driver()
             else:
-                driver = self.headless_chrome_driver()
-            self.logger.debug('Requesting ' + self.site_url)
-            driver.get(self.site_url)
-            self.logger.debug('Waiting for 15 seconds for website to load')
+                driver = self._headless_chrome_driver()
+            self._logger.debug('Requesting ' + self._site_url)
+            driver.get(self._site_url)
+            self._logger.debug('Waiting for 15 seconds for website to load')
             sleep(15)  # site is super slow to load
             # get api data
             for request in driver.requests:
                 if request.response and 'https://datastudio.google.com/embed/batchedDataV2' in request.path:
                     request_body_json = json.loads(request.body.decode('utf-8'))
                     if len(request_body_json['dataRequest']) == 5:
-                        self.logger.debug('Found request that contains required data')
+                        self._logger.debug('Found request that contains required data')
                         response_body_json = json.loads(request.response.body.decode('utf-8')[6:])
                         break
         except TimeoutException:
-            self.logger.error('Request timed out, check if {} is up.'.format(self.site_url))
+            self._logger.error('Request timed out, check if {} is up.'.format(self._site_url))
         except Exception as err:
-            self.logger.exception(err)
+            self._logger.exception(err)
         finally:
             if driver:
                 driver.quit()
         return response_body_json
 
     def scrape(self):
-        self.logger.debug('Data gathering started')
-        raw_data = self.get_data()
+        self._logger.debug('Data gathering started')
+        raw_data = self._get_data()
         if not raw_data:
-            self.logger.error("Didn't manage to find the data from requests")
+            self._logger.error("Didn't manage to find the data from requests")
             return
         data_response = raw_data['default']['dataResponse'][1]['dataSubset'][0]['dataset']['tableDataset']['column']
         dates = list(map(int, data_response[0]['stringColumn']['values']))
@@ -75,8 +75,8 @@ class Scrapper:
                                                      for i in range(1, 4))
         self.data = zip(dates, total_infected, total_cured, total_deaths)
 
-    def write_to_csv_file(self, *, destination='data.csv'):
-        self.logger.debug('Writing data to {}'.format(destination))
+    def write_to_csv_file(self, *, destination: str = 'data.csv'):
+        self._logger.debug('Writing data to {}'.format(destination))
         with open(destination, 'w', newline='') as f:
             csv_writer = csv.writer(f)
             for row in self.data:
